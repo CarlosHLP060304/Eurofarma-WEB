9d2ad0600abef7a95094bc4e08424381a2d3e0c7
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-07-25 17:59:03 -0300

    adicionada página de detalhamento do funcionário

--- js/script_usuarios/pesquisar_funcionarios.js
+++ js/script_usuarios/pesquisar_funcionarios.js
@@ -1,11 +1,15 @@
 import { getFuncionarios } from "./aluno_service.js";
 
+let funcionarios = []
+
 getFuncionarios().then(
                     dados=> document.querySelector("tbody").innerHTML =  dados.content.map(
                         dado=>{
+                            funcionarios.push(dado)
                             console.log(dado.tipo==="ALUNO")
                             if(dado.tipo === "ALUNO"){
-                            return `  <tr>
+                            return ` 
+                                    <tr id_aluno=${dado.id} class="tr-body">
                                         <td>${dado.nome}</td>
                                         <td>${dado.cpf}</td>
                                       </tr>` 
@@ -13,8 +17,14 @@ getFuncionarios().then(
                         
                         }
                 ).join("")
+    ).then(
+        ()=> document.querySelectorAll(".tr-body").forEach(
+            dado=> dado.addEventListener("click",()=>{
+                    window.location.href = `/pages/detalhamentoAluno.html?id_aluno=${dado.getAttribute("id_aluno")}`
+                }
             )
-
+        )
+    )
 
 
 
