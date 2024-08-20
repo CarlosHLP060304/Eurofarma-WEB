48355aec8ed0ff703090f950ec95da5d0fda894b
Carlos Henrique Lins Ponchirolli <107359480+CarlosHLP060304@users.noreply.github.com>
2024-08-20 16:15:41 -0300

    Adicionadas as partes que faltavam para enviar todos os dados da página de teste para o backend

--- js/script_teste/teste.js
+++ js/script_teste/teste.js
@@ -1,10 +1,17 @@
-const btn = document.querySelector("#btn_criar_questao")
+import { getTreinamentos } from "../script_treinamentos/treinamento_service.js"
+import { postQuestionario } from "./questionario_service.js"
+
+const btn_criar_questao = document.querySelector("#btn_criar_questao")
 const pop_up_criacao_questao = document.querySelector("#pop_up_criacao_questao")
 const tipo_questao = document.querySelector("#tipo_questao")
 const div_criacao_questao_especificada = document.querySelector("#div_criacao_questao_especificada")
 const close_pop_up_adicionar_questao = document.querySelector("#close_pop_up_adicionar_questao")
 const btn_adicionar_questao = document.querySelector("#btn-adicionar-questao")
+const select_treinamento =  document.querySelector("#select_treinamento")
+const btn_adicionar_questionario =  document.querySelector("#btn_adicionar_questionario")
 let questoes = []
+let questionario = {}
+
 function QuestaoObjetiva(){
     return `
         <label>Digite a pergunta da questão:</label>
@@ -18,7 +25,7 @@ function QuestaoObjetiva(){
     `
 }
 
-btn.addEventListener("click",()=>{
+btn_criar_questao.addEventListener("click",()=>{
     pop_up_criacao_questao.classList.remove("d-none")
 })
 
@@ -44,6 +51,7 @@ btn_adicionar_questao.addEventListener("click",()=>{
     dadosQuestao["resposta"] = document.querySelector("#resposta").value
     questoes.push(dadosQuestao) 
     console.log(questoes)
+    guardarDadosDB(questoes)
     exibirQuestoes(questoes)
 })
 
@@ -67,6 +75,7 @@ tipo_questao.addEventListener("change",()=>{
                 ${alternativas}
                 <p>Digite a resposta correta:</p>
                 <input type="text" name="resposta" id="resposta">`
+                
             }
         })
     }else{
@@ -90,3 +99,27 @@ function exibirQuestoes(questoes){
         }
     `
 }
+
+async function exibirTreinamentos(){
+    const pageable = await getTreinamentos()
+    const treinamentos = pageable.content
+    console.log(treinamentos)
+    select_treinamento.innerHTML = `
+        ${treinamentos.map(
+            treinamento => `
+                <option value=${treinamento.id}>${treinamento.nome}</option>
+            `
+        )}
+    `
+}
+
+
+function guardarDadosDB(questoes){
+    btn_adicionar_questionario.addEventListener("click",()=>{
+        questionario["id_treinamento"] =  parseInt(select_treinamento.value)
+        questionario["questoes"] = questoes
+        postQuestionario(questionario)
+    })
+}
+
+exibirTreinamentos()
