eaf7c0128d57515dac8156febff38009d350890e
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-09-04 15:57:57 -0300

    refactor: Refatorada a página de pesquisa dos funcionários

--- js/script_usuarios/pesquisar_funcionarios.js
+++ js/script_usuarios/pesquisar_funcionarios.js
@@ -7,29 +7,48 @@ getFuncionarios().then(
                         dado=>{
                             funcionarios.push(dado)
                             console.log(dado.tipo==="ALUNO")
-                            if(dado.tipo === "ALUNO"){
-                            return ` 
-                                    <tr id_aluno=${dado.id} class="tr-body">
-                                        <td>${dado.nome}</td>
-                                        <td>${dado.cpf}</td>
-                                        <td>${dado.re}</td>
-                                      </tr>` 
-                        }
-                        
+                            return returnAlunos(dado)
                         }
                 ).join("")
-    ).then(
-        ()=> document.querySelectorAll(".tr-body").forEach(
-            dado=> dado.addEventListener("click",()=>{
-                    window.location.href = `/pages/detalhamentoAluno.html?id_aluno=${dado.getAttribute("id_aluno")}`
-                }
-            )
-        )
-    )
+)
 
 
-document.querySelector("#btn_pesquisa_funcionario").addEventListener("click",()=>{
-    let pesquisa= document.querySelector("#pesquisa_funcionario").value
-    console.log(window.location.search)
-    window.location.search = `query=${pesquisa}`
+document.addEventListener("click",(e)=>{
+    const linha_funcionario_selecionada =  e.target.closest(".tr-body")
+    if(linha_funcionario_selecionada.classList.contains("tr-body")){
+        window.location.href = `/pages/detalhamentoAluno.html?id_aluno=${linha_funcionario_selecionada.getAttribute("id_aluno")}`    
+    }
 })
+
+document.getElementById("pesquisa_funcionario").addEventListener("input", async function() {
+    const query = this.value;
+
+    try {
+        const response = await fetch(`http://localhost:8080/usuario/research?query=${query}`);
+        const data = await response.json();
+
+        const list = document.querySelector("tbody");
+        list.innerHTML = `
+            ${data.map(item => {
+                return returnAlunos(item)
+            }).join(" ")
+            }
+        `
+
+    } catch (error) {
+        console.error('Erro ao buscar as sugestões:', error);
+    }
+});
+
+
+function returnAlunos(item){
+    if(item.tipo === "ALUNO"){
+        return `
+        <tr id_aluno=${item.id} class="tr-body">
+            <td>${item.nome}</td>
+            <td>${item.cpf}</td>
+            <td>${item.re}</td>
+            <td>${item.setor}</td>
+        </tr>`
+    }
+}
