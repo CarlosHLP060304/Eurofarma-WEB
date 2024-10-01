77cb9f7ed732125954b7336b51fe8056794edbd1
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-10-01 18:38:46 -0300

    env: trocado link do ngrok

--- js/enviroment/enviroment.js
+++ js/enviroment/enviroment.js
@@ -1,6 +1,6 @@
 // Função para determinar se está em ambiente de desenvolvimento ou produção
 export function returnBaseUrl() {
     let isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    return isLocalhost ? "http://localhost:8080" : "https://76e5-2804-431-c7fb-d612-f326-ca81-4eba-a90e.ngrok-free.app" 
+    return isLocalhost ? "http://localhost:8080" : "https://a06e-2804-7f0-b342-5554-b1db-5919-5cc6-7c12.ngrok-free.app" 
 }
 
