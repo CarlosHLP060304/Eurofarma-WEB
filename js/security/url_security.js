2c2f4aff9e2806780f06824e50ca4170c5b426af
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-10-13 19:35:48 -0300

    feat:Adicionada segurança nas urls

--- js/security/url_security.js
+++ js/security/url_security.js
@@ -1,3 +1,12 @@
-// function redirecionarParaPaginaDeLogin(params) {
-//     if(window.location)
-// }
+function redirecionarParaPaginaDeLogin() {
+    let token = localStorage.getItem("token") 
+    fetch(`http://localhost:8080/login?token=${token}`).then(
+        response =>{
+            if(window.location.pathname !== "/" && !response.ok){
+                window.location.pathname="/"
+            }
+        }
+    )    
+}
+
+redirecionarParaPaginaDeLogin()
