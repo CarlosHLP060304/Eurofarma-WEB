6e58f59e3dc1d3328a459ad781bd391e6e4a530d
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-10-14 16:17:14 -0300

    fix: arrumado fetch do método de redirecionarParaPaginaDeLogin

--- js/security/url_security.js
+++ js/security/url_security.js
@@ -2,7 +2,7 @@ import { returnBaseUrl } from "../enviroment/enviroment.js"
 
 function redirecionarParaPaginaDeLogin() {
     let token = localStorage.getItem("token") 
-    fetch(`${returnBaseUrl()}?token=${token}`).then(
+    fetch(`${returnBaseUrl()}/usuario?token=${token}`).then(
         response =>{
             if(window.location.pathname !== "/" && !response.ok){
                 window.location.pathname="/"
