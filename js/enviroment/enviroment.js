0d16684f1b86d70ae2184b82610e0f851b312c95
CarlosHLP060304 <carlosponchirolli655@gmail.com>
2024-10-24 21:38:53 -0300

    refactor:trocada url para ngrok

--- js/enviroment/enviroment.js
+++ js/enviroment/enviroment.js
@@ -1,6 +1,6 @@
 // Função para determinar se está em ambiente de desenvolvimento ou produção
 export function returnBaseUrl() {
     let isLocalhost = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1';
-    return isLocalhost ? "http://localhost:8080" : "https://treinamentos-eurofarma-api.onrender.com" 
+    return isLocalhost ? "http://localhost:8080" : "https://f620-201-0-22-186.ngrok-free.app" 
 }
 
